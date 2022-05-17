7a5f026f27e9e3a37233200fe380f5713263733d
Bruce07gt <94225931+Bruce07gt@users.noreply.github.com>
2022-05-17 14:55:30 -0300

    Upload

---
+++ Nota1&2/src/Notas/calcNotas.java
@@ -0,0 +1,19 @@
+package Notas;
+
+public class calcNotas {
+
+	public static void main(String[] args) {
+	
+		
+		int nota1 = 7;
+		int nota2 = 9;
+		int media = (nota1 + nota2) /2;
+			
+	if (media >=7) {
+			System.out.println("Aprovado");
+		} else {
+			System.out.println("Reprovado");
+		}
+			
+	}
+}
